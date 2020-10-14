7878ec80b0b9b8b9d36a1fb87932c328ec89e5e6
maperrea <maperrea@student.s19.be>
2020-10-14 23:26:31 +0200

    default fast commit message

--- push.sh
+++ push.sh
@@ -1,5 +1,11 @@
 #!/bin/zsh
 git add -A
+if [[ $1 == "-fast" ]]; then
+	git status -s
+	git commit -m "default fast commit message"
+	git push github master
+	exit
+fi
 git diff --cached --exit-code >/dev/null
 if [[ $? -eq 0 ]]; then
 	echo "No changes to be commited"
@@ -10,11 +16,13 @@ while true; do
 	echo -n "do you wish to push? [y/n]: "
 	read yn
 	case $yn in
-		Y | y ) echo -n "enter commit message: "
+		Y | y )
+			echo -n "enter commit message: "
 			read message
 			git commit -m "$message"
 			git push github master
 			exit;;
-		N | n ) exit;;
+		N | n )
+			exit;;
 	esac
 done
