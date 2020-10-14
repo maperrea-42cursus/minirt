08dbb952eb0e1328be948b7dca7bc793d2744199
maperrea <maperrea@student.s19.be>
2020-10-14 23:16:43 +0200

    push.sh test

--- push.sh
+++ push.sh
@@ -1,5 +1,10 @@
 #!/bin/zsh
 git add -A
+ret= git diff --cached --exit-code >/dev/null
+if [ $ret = 0 ]; then
+	echo "No changes to be commited"
+	exit
+fi
 git status -s
 while true; do
 	echo -n "do you wish to push? [y/n]: "
